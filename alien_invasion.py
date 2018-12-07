f8a023d767ad0ce9fb2291fc08f6aae858934391
voidyao <yaominghe@hotmail.com>
2018-12-07 16:04:42 +0800

    2

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from alien import Alien
 import game_functions as gf
@@ -13,10 +14,19 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
     #创建一艘飞船
     ship = Ship(ai_settings, screen)
     #创建一个用于存储子弹的编组
     bullets = Group()
+
+    #创建一个外星人编组
+    aliens = Group()
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
 
     #设置背景色
     bg_color = (230, 230, 230)
@@ -28,8 +38,9 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 
